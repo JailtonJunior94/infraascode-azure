9da499bcc289fc06d794ffa6593419db96020b83
jailton.junior <jailton.junior@iterative.com.br>
2021-09-17 19:05:25 -0300

    Criação de infra

--- src/aks.tf
+++ src/aks.tf
@@ -16,3 +16,34 @@ resource "azurerm_kubernetes_cluster" "finance_aks" {
     type = "SystemAssigned"
   }
 }
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.finance_aks.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.finance_aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.finance_aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.finance_aks.kube_config.0.cluster_ca_certificate)
+}
+
+resource "kubernetes_secret" "acr_secret" {
+  metadata {
+    name = "regcred"
+  }
+
+  data = {
+    ".dockerconfigjson" = <<DOCKER
+      {
+        "auths": {
+          "${azurerm_container_registry.finance_acr.login_server}": {
+            "auth": "${base64encode("${azurerm_container_registry.finance_acr.admin_username}:${azurerm_container_registry.finance_acr.admin_password}")}"
+          }
+        }
+      }
+      DOCKER
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+  depends_on = [
+    azurerm_container_registry.finance_acr,
+    azurerm_kubernetes_cluster.finance_aks
+  ]
+}
